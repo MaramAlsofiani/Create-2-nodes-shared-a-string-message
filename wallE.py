1f01a4c6f6034f39593b5bd038c80474831bc109
MaramAlsofiani <66621695+MaramAlsofiani@users.noreply.github.com>
2020-07-08 18:35:57 +0300

    Create wallE.py

---
+++ wallE.py
@@ -0,0 +1,17 @@
+import rospy
+from std_msgs.msg import String
+def wallE_talker():
+  hello_pub = rospy.Publisher('hello', String, queue_size=10)
+  rospy.init_node('wallE', anonymous=True)
+  rate = rospy.Rate(10)
+  while not rospy.is_shutdown():
+   greeting = "Hello, Eva!"
+   rospy.loginfo(greeting)
+   hello_pub.publish(greeting)
+   rate.sleep()
+
+if __name__== '__main__':
+    try:
+        wallE_talker()
+    except rospy.ROSInterruptException:
+         pass
